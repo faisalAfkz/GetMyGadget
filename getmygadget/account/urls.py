9952dd0b0614d15bfa53df58cf3d80b47c809cc9
Faisal-NSU <faisal.hossain03@northsouth.edu>
2020-12-27 16:33:02 +0600

    changed url paths for action and account

--- getmygadget/account/urls.py
+++ getmygadget/account/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login_view, name='login.html'),
-    path('signup', views.signup, name='signup.html'),
+    path('login/', views.login_view, name='login.html'),
+    path('signup/', views.signup, name='signup.html'),
     ]
--- getmygadget/action/urls.py
+++ getmygadget/action/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('trend', views.trend, name='trend.html'),
+    path('trend/', views.trend, name='trend.html'),
     ]
