9281a118dec1506adc9be6e83fc3078b96f45279
Ahmed_1901 <ahmed.jawad01@northsouth.edu>
2020-12-30 21:25:02 +0600

    +signupAuth Query +fix +errorMsg +url redirect

--- getmygadget/account/views.py
+++ getmygadget/account/views.py
@@ -19,6 +19,9 @@ def login_view(request):
             request.session['location'] = username_object.location
             firstname_context = request.session['firstName']
             return render(request, 'account/home.html', {'firstname_context': firstname_context})
+        else:
+            message = "Email or password did not match."
+            return render(request, 'account/login.html', {'message': message})
 
         # if password in user_ojbectList:
         #    sessionUsernameToStore = [u.__str__ for u in user_ojbectList]
@@ -40,12 +43,23 @@ def login_view(request):
 
 def signup(request):
     form = CreateUserForm()
-
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
+        # email = form.POST.cleaned_data['email']
         if form.is_valid():
-            form.save()
-            return render(request, 'account/home.html')
+            email = form.cleaned_data["email"]
+            user_object = GeneralUser.objects.raw('SELECT * FROM account_generaluser WHERE email = %s', [email])
+            if user_object:
+                message = "User with the following email already exists."
+                context = {'form': form, 'message': message}
+                return render(request, 'account/signup.html', context)
+            else:
+                form.save()
+                # message = "Account Created Successfully."
+                # context = {'message': message}
+                # return render(request, 'account/login.html', context) //rendering does not Redirect to to login
+                return redirect("/account/login")
 
     context = {'form': form}
     return render(request, 'account/signup.html', context)
+
