9763eb727f2115a30ffdf70953ead37bba6ec0a3
Faisal-NSU <faisal.hossain03@northsouth.edu>
2021-01-01 12:11:37 +0600

    added STARTECH search system

--- getmygadget/search/models.py
+++ getmygadget/search/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from abc import ABC,abstractmethod
+from bs4 import BeautifulSoup
+import requests
+import re
 
-# Create your models here.
+class ScrapSearch(ABC):
+    @abstractmethod
+    def fetch(self):
+        pass
+
+class Web1(ScrapSearch):
+    url = "https://www.startech.com.bd/product/search?search="
+
+    def addSearch(self,search):
+        search = search.replace(" ", "%20")
+        self.url = self.url + search
+
+    def fetch(self):
+        name = []
+        price = []
+        link = []
+        img = []
+        response = requests.get(self.url)
+        data = response.text
+        soup = BeautifulSoup(data, 'html.parser')
+
+        products = soup.find_all('div', {'class': 'product-thumb'})
+
+        for p in products:
+            name.append(p.find('h4', {'class': 'product-name'}).text)
+            temp = p.find('div', {'class': 'price'}).text
+            temp = re.sub('\W+', '', temp)
+            price.append(temp)
+            link.append(p.find('a').get('href'))
+            img.append(p.find('img').get('src'))
+
+        return name, price, link, img
