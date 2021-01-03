6543ac75aa822366113a39fcf592d9106b0b93c4
Ahmed_1901 <ahmed.jawad01@northsouth.edu>
2021-01-04 03:11:25 +0600

    clearing old files from new_LoginSystem

--- getmygadget/account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_view, name='login.html'),
-    path('signup/', views.signup, name='signup.html'),
-    path('home/', views.home, name='home.html'),
-    ]
--- getmygadget/trend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.trend, name='trend.html'),
-    ]
